518f59a6b44251bd0bc1fb04720d0def51295f4f
Johan Bloemberg <github@ijohan.nl>
2020-11-05 18:59:59 +0100

    Fix priority to serve application path before frontend.

--- Boltdir/site-modules/dashboard/manifests/app.pp
+++ Boltdir/site-modules/dashboard/manifests/app.pp
@@ -54,9 +54,9 @@ class dashboard::app (
     health_check_interval => 60,
     labels                => [
       'traefik.enable=true',
-      'traefik.frontend.priority=10',
+      'traefik.frontend.priority=20',
       # all dynamic content should be served by Django, otherwise fallback to static content
-      "traefik.frontend.rule=\"Host:${_hosts}; Path: /data,/logout,/session,/upload,/mail,/account,/admin,/jet\"",
+      "traefik.frontend.rule=\"Host:${_hosts};PathPrefix: /data,/logout,/session,/upload,/mail,/account,/admin,/jet\"",
       "\"traefik.frontend.headers.customResponseHeaders=${headers}\"",
     ],
     env                   => [
