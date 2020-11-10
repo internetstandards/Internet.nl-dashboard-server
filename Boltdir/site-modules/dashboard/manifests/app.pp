8203eae6352f540fa21595771cda2e041ab05c10
Johan Bloemberg <github@ijohan.nl>
2020-11-10 18:45:03 +0100

    Make list of dynamic paths easier to maintain.

--- Boltdir/site-modules/dashboard/manifests/app.pp
+++ Boltdir/site-modules/dashboard/manifests/app.pp
@@ -47,6 +47,19 @@ class dashboard::app (
     ],
   }
 
+  # all paths that should be routed to Django dynamic backend
+  $dynamic_content_paths = [
+    '/data',
+    '/logout',
+    '/session',
+    '/upload',
+    '/mail',
+    '/account',
+    '/admin',
+    '/jet',
+    '/static',
+  ]
+
   ::docker::run { 'dashboard':
     image                 => "internetstandards/dashboard:${image_tag}",
     systemd_restart       => always,
@@ -56,7 +69,7 @@ class dashboard::app (
       'traefik.enable=true',
       'traefik.frontend.priority=20',
       # all dynamic content should be served by Django, otherwise fallback to static content
-      "traefik.frontend.rule=\"Host:${_hosts};PathPrefix: /data,/logout,/session,/upload,/mail,/account,/admin,/jet,/static_frontend,/static\"",
+      "traefik.frontend.rule=\"Host:${_hosts};PathPrefix:${join($dynamic_content_paths,',')}\"",
       "\"traefik.frontend.headers.customResponseHeaders=${headers}\"",
     ],
     env                   => [
